20b722210def337c364a3f88ece86dd338fcd6f1
Kinson <46670042+KinsonLiang@users.noreply.github.com>
2022-12-07 22:23:57 -0800

    Lab 8 Create

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Lab 8 - Starter
+
+**Jiancheng Liang no teammate**
+
+
+1. Option 1 is correct as it will ensure that any code that is pushed will have to go through some quality checks so that the code is working before moving onto the next part of development. Manually running tests would work, but would be inefficient and a perfect task for automation libraries like Jest to do.
+
+2. No, we should use a unit test for it. Unit tests can ensure the individual components and functions can be returning the correct values.
+
+3. Unit tests can only determine whether a particular output of that function is correct. With E2E style tests, messaging functions can be better tested because conveying a message to another user involves multiple steps. These steps can include getting the right user, sending and receiving the right message, and so on
+
+4. Yes, this feature has only one part, message length, which can be easily verified by checking for strings containing messages of length 80 or less
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,103 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+test('validPhoneNumberA', () => {
+    phoneNumber = '408-602-8728'
+    expect(functions.isPhoneNumber(phoneNumber)).toBe(true);
+});
+
+test('validPhoneNumberB', () => {
+    phoneNumber = '458-757-4803'
+    expect(functions.isPhoneNumber(phoneNumber)).toBe(true);
+});
+
+test('notvalidPhoneNumberA', () => {
+    phoneNumber = 'ABC-DEF-XYTZ'
+    expect(functions.isPhoneNumber(phoneNumber)).toBe(false);
+});
+
+test('notvalidPhoneNumberB', () => {
+    phoneNumber = 'XXX-XXX-XXXX'
+    expect(functions.isPhoneNumber(phoneNumber)).toBe(false);
+});
+
+test('validEmailA', () => {
+    emailString = 'zbc@outlook.com'
+    expect(functions.isEmail(emailString)).toBe(true);
+});
+
+test('validEmailB', () => {
+    emailString = 'vcxv@gmail.com'
+    expect(functions.isEmail(emailString)).toBe(true);
+});
+
+test('notvalidEmailA', () => {
+    emailString = 'abcd@com.com';
+    expect(functions.isEmail(emailString)).toBe(false);
+});
+
+test('notvalidEmailB', () => {
+    emailString = 'abcdfe.dsad.com'
+    expect(functions.isEmail(emailString)).toBe(false);
+});
+
+test('strongPasswordA', () => {
+    passString = 'strong_password!!!'
+    expect(functions.isStrongPassword(passString)).toBe(true);
+});
+
+test('strongPasswordB', () => {
+    passString = 'abc_cdf.'
+    expect(functions.isStrongPassword(passString)).toBe(true);
+});
+
+test('weakPasswordA', () => {
+    passString = '123';
+    expect(functions.isStrongPassword(passString)).toBe(false);
+});
+
+test('weakPasswordB', () => {
+    passString = '1234567889qwertyuiopkg'
+    expect(functions.isStrongPassword(passString)).toBe(false);
+});
+
+test('validDateA', () => {
+    date = '20/11/2020'
+    expect(functions.isDate(date)).toBe(true);
+});
+
+test('validDateB', () => {
+    date = '09/19/1999'
+    expect(functions.isDate(date)).toBe(true);
+});
+
+test('notvalidDateA', () => {
+    date = '1010/1010';
+    expect(functions.isDate(date)).toBe(false);
+});
+
+test('notvalidDateB', () => {
+    date = '12072022'
+    expect(functions.isDate(date)).toBe(false);
+});
+
+test('validHexA', () => {
+    hexString = '#12F'
+    expect(functions.isHexColor(hexString)).toBe(true);
+});
+
+test('validHexB', () => {
+    hexString = '#FFFFFFC'
+    expect(functions.isHexColor(hexString)).toBe(true);
+});
+
+test('notvalidHexA', () => {
+    hexString = 'ABC';
+    expect(functions.isHexColor(hexString)).toBe(false);
+});
+
+test('notvalidHexB', () => {
+    hexString = '#F'
+    expect(functions.isHexColor(hexString)).toBe(false);
+});
